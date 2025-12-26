be0f6808b3cb1f16d2de7600b962c368dd5ec0d0
Mohd. Suleman <sulemnmohd345@gmail.com>
2025-12-26 23:12:14 +0530

    minor bug fix

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -28,10 +28,11 @@ const client = new openai_1.default({
     baseURL: "https://api.groq.com/openai/v1",
 });
 app.post('/template', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c;
     const prompt = req.body.prompt;
-    const response = yield client.responses.create({
+    const response = yield client.chat.completions.create({
         model: "openai/gpt-oss-20b",
-        input: [
+        messages: [
             {
                 role: "user",
                 content: prompt
@@ -42,7 +43,8 @@ app.post('/template', (req, res) => __awaiter(void 0, void 0, void 0, function* 
             }
         ]
     });
-    const answer = response.output_text;
+    const answer = (_c = (_b = (_a = response === null || response === void 0 ? void 0 : response.choices[0]) === null || _a === void 0 ? void 0 : _a.message) === null || _b === void 0 ? void 0 : _b.content) === null || _c === void 0 ? void 0 : _c.trim();
+    console.log(answer);
     if (answer === "react") {
         res.json({
             prompts: [prompts_1.BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
@@ -52,7 +54,7 @@ app.post('/template', (req, res) => __awaiter(void 0, void 0, void 0, function* 
     }
     if (answer === "node") {
         res.json({
-            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${react_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${node_1.basePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
             uiPrompts: [node_1.basePrompt]
         });
         return;
@@ -61,11 +63,12 @@ app.post('/template', (req, res) => __awaiter(void 0, void 0, void 0, function* 
     return;
 }));
 app.post('/chat', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c;
     const messages = req.body.messages;
     // console.log(messages)
-    const response = yield client.responses.create({
+    const response = yield client.chat.completions.create({
         model: "openai/gpt-oss-20b",
-        input: [
+        messages: [
             ...messages,
             {
                 role: "system",
@@ -73,41 +76,12 @@ app.post('/chat', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             }
         ]
     });
-    // // console.log(messages)
-    // console.log(response.choices[0].message.content)
+    const answer = (_c = (_b = (_a = response === null || response === void 0 ? void 0 : response.choices[0]) === null || _a === void 0 ? void 0 : _a.message) === null || _b === void 0 ? void 0 : _b.content) === null || _c === void 0 ? void 0 : _c.trim();
     res.json({
-        response: response.output_text
+        answer
     });
     return;
 }));
 app.listen(port, () => {
     console.log(`server is running on Port ${port}`);
 });
-// async function main() {
-//   const stream = await openai.chat.completions.create({
-//     model: "openai/gpt-oss-20b:free",
-//     messages: [
-//       {
-//         role: "system",
-//         content: getSystemPrompt()
-//       },
-//       {
-//         role: "user",
-//         content: ""
-//       },
-//       {
-//         role: "user",
-//         content: "build a todo application"
-//       }
-//     ],
-//     stream: true
-//   });
-//   // console.log(msg.choices[0].message.content);
-//   for await (const chunk of stream) {
-//     const content = chunk.choices[0]?.delta?.content;
-//     if (content) {
-//       process.stdout.write(content); // print without adding a new line each time
-//     }
-//   }
-// }
-// main();
